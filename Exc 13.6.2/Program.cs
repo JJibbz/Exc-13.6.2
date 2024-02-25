25496cdc3f57ed3654a4e14270324794361ec19a
JJibbz <vkazancev322@gmail.com>
2024-02-25 19:32:41 +0700

    Промежуточное решение

--- Exc 13.6.2/Program.cs
+++ Exc 13.6.2/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            
+            string text = File.ReadAllText(@"C:\Users\lino-\OneDrive\Рабочий стол\Text1.txt");
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            GetQuantityofWords(noPunctuationText);
+
         }
+
+        static Dictionary<string, int> GetQuantityofWords(string text)
+        {
+            var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var uniqueWords = new HashSet<string>();
+            var quantityOfWords = new Dictionary<string, int>();
+            int counter;
+            foreach (string word in words)
+            {
+                uniqueWords.Add(word);
+            }
+            
+            foreach (string word in uniqueWords)
+            {
+                counter = 0;
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (word == words[i])
+                    {
+                        counter++;
+                    }
+                }
+                quantityOfWords.Add(word, counter);
+            }
+            return quantityOfWords;
+        }
+
+     
     }
 }
